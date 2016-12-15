8f575b3fe05fc2bd6902cfbd6f07d4fcff563836
bot <ims03@mitsukoshiisetanshisutemusoryushonzu-no-MacBook-Pro-2.local>
2016-12-15 17:44:51 +0900

    new src commit

--- server.js
+++ server.js
@@ -39,9 +39,25 @@ var intents = new builder.IntentDialog({
 // Bots Dialogs
 //=========================================================
 
-// 初期ダイアログをintentDialogとして使用する
 bot.dialog('/', intents);
-
+process.on('uncaughtException',function(err){console.log(err);});
+function getWikipedia(opt) {
+	return new Promise(function(resolve,reject) {
+		request.get(opt, function (error,response,body) {
+			if (!error & response.statusCode == 200) {
+				if (body) {
+					var tbody = body[0].body;
+					tbody = tbody.substring(0,tbody.indexOf("。"));
+					return resolve(tbody);
+				} else {
+					return reject('error');
+				}
+			} else {
+				return reject('Error Occured code : ' + response.statusCode);
+			}
+		});
+	});
+}
 intents.matches('isWeather',
     function (session, args) {
         var city = builder.EntityRecognizer.findEntity(args.entities, 'City');
@@ -51,69 +67,77 @@ intents.matches('isWeather',
 		var forecastArea = "";
         if (city) {
 			forecastArea = city.entity.replace(/\s+/g,"");
-			forecast.get(forecastArea).then(function(forecast) {
-				if (day) {
-					if (day.entity == '明日' || day.entity == 'あした' || day.entity == 'あす') {
-						forecastResult = forecast.tomorrow.text;
-						resultText += '明日の';
-					} else if (day.entity == '今日' || day.entity == '本日' || day.entity == 'きょう') {
-						forecastResult = forecast.today.text;
-						resultText += '今日の';
+			try {
+				forecast.get(forecastArea).then(function(forecast) {
+					if (day) {
+						if (day.entity == '明日' || day.entity == 'あした' || day.entity == 'あす') {
+							forecastResult = forecast.tomorrow.text;
+							resultText += '明日の';
+						} else if (day.entity == '今日' || day.entity == '本日' || day.entity == 'きょう') {
+							forecastResult = forecast.today.text;
+							resultText += '今日の';
+						} else {
+							session.endDialog("今日か明日の天気しかわかりませんが何か？万能じゃないんで。");
+						}
+						resultText += forecast.where + "の天気は" + forecastResult + "なんじゃない？あとは気象庁に聞いてくれ。";
+						session.endDialog(resultText);
 					} else {
-						session.endDialog("今日か明日の天気しかわかりませんが何か？万能じゃないんで。");
+						session.endDialog("今日か明日くらい言ってくれてもいいんじゃない？");
 					}
-					resultText += forecast.where + "の天気は" + forecastResult + "なんじゃない？あとは気象庁に聞いてくれ。";
-					session.endDialog(resultText);
-				} else {
-					session.endDialog("今日か明日くらい言ってくれてもいいんじゃない？");
-				}
-			}).catch(function(err) {
-				session.endDialog("何故か例外をキャッチしたよ。ぼく死ぬのかな？");
-			});
+				}).catch(function(err) {
+					session.endDialog("何故か例外をキャッチしたよ。ぼく死ぬのかな？");
+				});
+			} catch(err) {
+				session.endDialog("その場所の天気は知らん。知らんもんは知らん。");
+			}
 		} else {
 			session.endDialog("都道府県ぐらい言ってほしいもんだ。");
 		}
     }
 )
-.matches('getWeather', 
-    function (session, args) {
-        var city = builder.EntityRecognizer.findEntity(args.entities, 'City');
-        var day = builder.EntityRecognizer.findEntity(args.entities, 'Day');
+.matches('getWeather', [
+	function (session, args) {
+		var city = builder.EntityRecognizer.findEntity(args.entities, 'City');
+		var day = builder.EntityRecognizer.findEntity(args.entities, 'Day');
 		var forecastResult = "";
 		var resultText = "";
 		var forecastArea = "";
-        if (city) {
+		if (city) {
 			forecastArea = city.entity.replace(/\s+/g,"");
-			forecast.get(forecastArea).then(function(forecast) {
-				if (day) {
-					if (day.entity == '明日' || day.entity == 'あした' || day.entity == 'あす') {
-						forecastResult = forecast.tomorrow.text;
-						resultText += '明日の';
-					} else if (day.entity == '今日' || day.entity == '本日' || day.entity == 'きょう') {
-						forecastResult = forecast.today.text;
-						resultText += '今日の';
+			try {
+				forecast.get(forecastArea).then(function(forecast) {
+					if (day) {
+						if (day.entity == '明日' || day.entity == 'あした' || day.entity == 'あす') {
+							forecastResult = forecast.tomorrow.text;
+							resultText += '明日の';
+						} else if (day.entity == '今日' || day.entity == '本日' || day.entity == 'きょう') {
+							forecastResult = forecast.today.text;
+							resultText += '今日の';
+						} else {
+							session.endDialog("今日か明日の天気しかわかりませんが何か？万能じゃないんで。");
+						}
+						resultText += forecast.where + "の天気は" + forecastResult + "だと思うよ。勘だけども。";
+						session.endDialog(resultText);
 					} else {
-						session.endDialog("今日か明日の天気しかわかりませんが何か？万能じゃないんで。");
+						session.endDialog("今日か明日くらい言ってくれてもいいんじゃない？");
 					}
-					resultText += forecast.where + "の天気は" + forecastResult + "だと思うよ。勘だけども。";
-					session.endDialog(resultText);
-				} else {
-					session.endDialog("今日か明日くらい言ってくれてもいいんじゃない？");
-				}
-			}).catch(function(err) {
-				session.endDialog("何故か例外をキャッチしたよ。ぼく死ぬのかな？");
-			});
+				}).catch(function(err) {
+					session.endDialog("何故か例外をキャッチしたよ。ぼく死ぬのかな？");
+				});
+			} catch(err) {
+				session.endDialog("その場所の天気は知らん。知らんもんは知らん。");
+			}
 		} else {
 			session.endDialog("都道府県ぐらい言ってほしいもんだ。");
 		}
-    }
-)
-.matches('whois', 
+	}
+])
+.matches('whois', [
     function (session, args) {
 		session.endDialog("はじめまして僕はbot。名前はまだない。日本の天気に詳しいよ。");
 	}
-)
-.matches('replyHello', 
+])
+.matches('replyHello', [
     function (session, args) {
 		var greet = builder.EntityRecognizer.findEntity(args.entities, 'Greetings');
 		if (greet) {
@@ -122,8 +146,8 @@ intents.matches('isWeather',
 			session.endDialog("ん？よ、よお。（。。。よくわからないけど挨拶っぽい）");
 		}
 	}
-)
-.matches('replyGoodBye', 
+])
+.matches('replyGoodBye', [
     function (session, args) {
 		var greet = builder.EntityRecognizer.findEntity(args.entities, 'Greetings');
 		if (greet) {
@@ -132,7 +156,29 @@ intents.matches('isWeather',
 			session.endDialog("別れの挨拶を言われている気がする。ではまたね。");
 		}
 	}
-)
-.onDefault(function (session) { session.send("日本語でok") });
-
+])
+.matches('whatmeans', [
+	function (session, args) {
+		var words = builder.EntityRecognizer.findAllEntities(args.entities, 'Word');
+		if (words) {
+			var word = '';
+			for (var i=0; i < words.length; i++) {
+				word = word + words[i].entity;
+			}
+			word = encodeURI(word);
+			var options = {
+				url:'http://wikipedia.simpleapi.net/api?output=json&q=' + word,
+				json:true
+			};
+			getWikipedia(options).then(function(result) {
+				session.endDialog(result + '　ということだね。多分ね。');
+			}).catch(function(reject) {
+				session.endDialog('そんな難しいこと言われても。。。');
+			});
+		} else {
+			session.endDialog("時々自分が何を考えているかわからなくなるとです。");
+		}
+	}
+])
+.onDefault(function (session) { session.endDialog("日本語でok") });
 
